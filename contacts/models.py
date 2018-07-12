73d85a1b7ac04db41254b10e4354af21dc2ff658
Hugo Corra <hugocorra@gmail.com>
2018-07-12 20:05:57 -0300

    resolvidos pequenos problemas nos modelos

--- contacts/models.py
+++ contacts/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class Contato(models.Model):
+    TIPO_CONTATO = (
+        ('P', 'Pessoal'),
+        ('C', 'Comercial'),
+    )
+
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    ocupação = models.CharField(max_length=50, verbose_name='Ocupação')
+    tipo = models.CharField(choices=TIPO_CONTATO, max_length=1, verbose_name='Tipo')
+
+
 class Endereco(models.Model):
     pais = models.CharField(max_length=50, verbose_name='País')
     estado = models.CharField(max_length=30, verbose_name='Estado')
@@ -15,25 +26,10 @@ class Telefone(models.Model):
     numero = models.CharField(max_length=20, verbose_name='Telefone')
 
 
-class Contato(models.Model):
-    TIPO_CONTATO = (
-        ('P', 'Pessoal'),
-        ('C', 'Comercial'),
-    )
-
-    nome = models.CharField(max_length=100, verbose_name='Nome')
-    ocupação = models.CharField(max_length=50, verbose_name='Ocupação')
-    cargo = models.CharField()
-    tipo = models.CharField(choices=TIPO_CONTATO, max_length=1, verbose_name='Tipo')
-
-
 class ContatoEndereco(models.Model):
     contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
     #tipo = Residencial, Comercial, Outro
-
-    class Meta:
-        ordering = ('nome',)
 
 
 class Instituicao(models.Model):
@@ -62,13 +58,7 @@ class InstituicaoEndereco(models.Model):
 
 class DepartamentoEndereco(models.Model):
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    endereco = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-
-
-class Notas(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTime(auto_now=False, auto_now_add=False)
-    texto = models.TextField(verbose_name='Nota')
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
 
 class InstituicaoTelefone(models.Model):
@@ -80,3 +70,8 @@ class DepartamentoTelefone(models.Model):
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     telefone = models.ForeignKey(Telefone, on_delete=models.CASCADE)
 
+
+# class Notas(models.Model):
+#     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+#     timestamp = models.DateTime(auto_now=False, auto_now_add=False)
+#     texto = models.TextField(verbose_name='Nota')
