02708e87ba760e3a0d63812b75ae0582b64eee28
Hugo Corra <hugocorra@gmail.com>
2018-07-12 13:37:44 -0300

    adicionado esquema de serializacao

--- contacts/models.py
+++ contacts/models.py
@@ -20,47 +20,56 @@ class Contato(models.Model):
     cargo = models.CharField()
     #tipo = Pessoal, Comercial
 
+    class Meta:
+        ordering = ('nome',)
 
-class ContatoEndereco(models.Model)
-    contato = models.ForeignKey(Contato)
-    endereco = models.ForeignKey(Endereco)
+
+class ContatoEndereco(models.Model):
+    contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
     #tipo = Residencial, Comercial, Outro
 
 
 class ContatoTelefone(models.Model):
-    contato = models.ForeignKey(Contato)
-    telefone = models.ForeignKey(Telefone)
+    contato = models.ForeignKey(Contato, on_delete=models.CASCADE)
+    telefone = models.ForeignKey(Telefone, on_delete=models.CASCADE)
 
 
 class Instituicao(models.Model):
     nome = models.CharField()
 
+    class Meta:
+        ordering = ('nome',)
+
 
 class Departamento(models.Model):
     nome = models.CharField()
 
+    class Meta:
+        ordering = ('nome',)
+
 
 class InstituicaoEndereco(models.Model):
-    instituicao = models.ForeignKey(Instituicao)
-    endereco = models.ForeignKey(Endereco)
+    instituicao = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
 
 class DepartamentoEndereco(models.Model):
-    departamento = models.ForeignKey(Departamento)
-    endereco = models.ForeignKey(Departamento)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
 
 class InstituicaoTelefone(models.Model):
-    instituicao = models.ForeignKey(Instituicao)
-    telefone = models.ForeignKey(Telefone)
+    instituicao = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
+    telefone = models.ForeignKey(Telefone, on_delete=models.CASCADE)
 
 
 class DepartamentoTelefone(models.Model):
-    departamento = models.ForeignKey(Departamento)
-    telefone = models.ForeignKey(Telefone)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    telefone = models.ForeignKey(Telefone, on_delete=models.CASCADE)
 
 
-class Notas(models.Model):
-    usuario = models.ForeignKey(User)
-    timestamp = models.DateTime()
-    texto = models.TextField()
+# class Notas(models.Model):
+#     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+#     timestamp = models.DateTime()
+#     texto = models.TextField()
--- contacts/urls.py
+++ contacts/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from contacts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('contacts/', include('contacts.urls'))
+    path('', views.home, name='home')
 ]
