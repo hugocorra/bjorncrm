2d220f1abba5440a9dc55886d5d2e91d138c890b
Hugo Corra <hugocorra@gmail.com>
2018-10-15 18:28:14 -0300

    aprimoramentos interface de interacoes

--- interaction/forms.py
+++ interaction/forms.py
@@ -1,9 +1,13 @@
 from django.forms import ModelForm
 from interaction import models
 
+
 class InteracaoForm(ModelForm):
     class Meta:
         model = models.Interacao
         fields = '__all__'
 
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
